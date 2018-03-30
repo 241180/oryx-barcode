b4199130f9f7015475bc8461db86807bf159252d
Sami Mbarki <sami.mbarki@gefco.net>
2018-03-30 13:51:43 +0100

    first commit

--- app/src/main/java/com/javacodegeeks/oryx/OryxBarcodeReader.java
+++ app/src/main/java/com/javacodegeeks/oryx/OryxBarcodeReader.java
@@ -7,12 +7,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OryxBarcodeReader extends Activity {
+public class OryxBarcodeReader extends AppCompatActivity {
     /**
      * Called when the activity is first created.
      */
@@ -56,6 +59,33 @@ public class OryxBarcodeReader extends Activity {
         }
 
         super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void scanBar(View v) {
