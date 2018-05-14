b5fb394a651f7b2251d98cfaf981e4efce6010da
Sami Mbarki <sami.mbarki@gefco.net>
2018-05-14 16:22:04 +0100

    first commit

--- app/src/main/java/com/oryx/barcode/context/IProduct.java
+++
@@ -1,7 +0,0 @@
-package com.oryx.barcode.context;
-
-public class IProduct {
-    public static String code = null;
-    public static String name = null;
-    public static String description = null;
-}
